9fbf3ad21597404fdb1a3b46d36681e4d8f89678
Gar1s <partyka.eduard@gmail.com>
2023-10-08 23:39:14 +0300

    task3-4

--- 1/client.py
+++ 1/client.py
@@ -3,13 +3,11 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 5000))
 
-send_data = input("Enter: ")
-client.send(send_data.encode("utf-8"))
+while True:
+	send_data = input("Enter: ")
+	client.send(send_data.encode("utf-8"))
+	data = client.recv(1024).decode("utf-8")
+	if(data == "exit"):
+		break
 
-data = client.recv(1024).decode("utf-8")
-if data:
-	print("data = ", data)
-	client.send("Data is cleared!".encode("utf-8"))
-else: 
-	client.send("Data is damaged!".encode("utf-8"))
 client.close()
--- 1/server.py
+++ 1/server.py
@@ -1,6 +1,4 @@
 import socket
-import datetime
-import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -11,14 +9,12 @@ while True:
 	user, address = server.accept()
 	print("Connected by ", address)
 
-	data = user.recv(1024).decode("utf-8")
-	time.sleep(5)
-	print("received data from client: ", data)
-
-	user.send(data.encode("utf-8"))
-
-	response = user.recv(1024).decode("utf-8")
-	print("response: ", response)
-	
+	while True:
+		data = user.recv(1024).decode("utf-8")
+		print("received data from client: ", data)
+		user.send(data.encode("utf-8"))
+		if(data == "exit"):
+			break
+			
 	user.close()
 server.close()
